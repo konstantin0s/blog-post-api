701b564f7d1ffca057cf0cd0c30e15253556d4a4
konstantin0s <constantintofan85@gmail.com>
2019-03-16 20:58:49 +0100

    Add users profile router and moving to front end to install axios and jwt

--- routes/Users.js
+++ routes/Users.js
@@ -73,4 +73,22 @@ users.post('/login', (req, res) => {
   })
 })
 
+users.get('/profile', (req, res) => {
+  const decoded = jwt.verify(req.headers['authorization'], process.env.SECRET_KEY)
+
+  User.findOne({
+    _id: decoded._id
+  })
+  .then(user => {
+    if (user) {
+      res.json(user)
+    } else {
+      res.send('User does not exist')
+    }
+  })
+  .catch(err => {
+    res.send('error: ' + err)
+  })
+})
+
 module.exports = users;
